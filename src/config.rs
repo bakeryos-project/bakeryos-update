3ea4cb9f73b9b30b23cae58ce4e8a0218aff5315
smtdfc <admin.smtdfc@gmail.com>
2026-08-29 18:08:49 +0700

    fix: fix D-bus service issue

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
 pub static VERSION: &str = "0.1.0";
 pub static GETTEXT_PACKAGE: &str = "bakeryos-update";
-pub static LOCALEDIR: &str = "/home/smtdfc/Projects/.gnome-builder/projects/bakeryos-update/install/host/share/locale";
-pub static PKGDATADIR: &str = "/home/smtdfc/Projects/.gnome-builder/projects/bakeryos-update/install/host/share/bakeryos-update";
+pub static LOCALEDIR: &str = "/usr/share/locale";
+pub static PKGDATADIR: &str = "/usr/share/bakeryos-update";
